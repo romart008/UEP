7ba1624d6eacd747e223889457111bbdc0a9532c
romart008 <martunykr9@gmail.com>
2025-07-23 14:05:24 +0300

    One singular button, and now i have to add browser driver and a library to press it

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Universal Entry Program
 This is specialized app for Ukrainian student to help with entering University
+
+Usable only if you have Edge Browser.
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
+import os
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.edge.options import Options as EdgeOptions
+
 url = 'https://vstup.osvita.ua/y2025/r14/97/1483051/'
-
 entry_data = {}
+table = []
 
+# Data for number of positions and etc.
 def Get_Entry_Data(soup):
     
     entry_data_div = soup.find_all('div', class_='table-of-specs-item panel-mobile')
 
-    # Data for number of positions and etc.
     if entry_data_div:
 
         value_tags = entry_data_div[2].find_all('b')        # [2] for needed data
@@ -21,7 +26,25 @@ def Get_Entry_Data(soup):
             if label:
                 entry_data[label.replace(':', '')] = value
     
-    print(entry_data)
+    return entry_data
+
+# Getting Data of All participants
+def Pos_Table():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    driver_path = os.path.join(base_dir, 'msedgedriver.exe')
+
+    options = EdgeOptions()
+    options.add_argument("--headless") # Background mode
+    options.add_argument("window-size=1920,1080")
+    service = EdgeService(executable_path=driver_path)
+
+
+    driver = webdriver.Edge(service=service, options=options)
+    print("Edge started in background...")
+
+
+    driver.quit()
+    print("Work Ended.")
 
 try:
     response = requests.get(url)
@@ -29,7 +52,10 @@ try:
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    Get_Entry_Data(soup)
+    print(Get_Entry_Data(soup))
+
+    Pos_Table()
 
 except requests.exceptions.RequestException as e:
     print(f"Помилка під час запиту: {e}")
+    
